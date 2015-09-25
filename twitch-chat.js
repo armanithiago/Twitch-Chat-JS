39c4a269e7c17d3fb8fbbdd5b7a119560ecb9573
armanithiago <armani.thiago@gmail.com>
2015-09-25 14:26:55 -0300

    V1.01
    - Add variables to method for decides texts and seconds
    - Can create multiples spammers
    - Can clean specific spammer or all
    - List the actives spammers (only number or names)

--- twitch-chat.js
+++ twitch-chat.js
@@ -1,13 +1,40 @@
-function startFarmer(){
-  var seconds = 0
-  var text = 'OSbury TriHard OSbury' 
-  var chatspammer = setInterval(function() {
-    seconds = seconds + 1;
-    $('#chat_text_input').val(text);
-    $('[class="button primary float-right send-chat-button"]').click();
-  }, 30000);
+var GLOBAL = {
+	chatSpammer : [],
+	numActivesInterval : 0
 }
 
-function stopFarmer(){
-  clearInterval(chatspammer);
+function startSpammer(text,seconds){
+	var secondsInterval = seconds*1000;
+	var string = text;
+	GLOBAL.chatSpammer.push({inverval:setInterval(function() {
+		$('#chat_text_input').val(string);
+		$('[class="button primary float-right send-chat-button"]').click();
+	}, secondsInterval),intervalTitle:string});
+	GLOBAL.numActivesInterval++;
+}
+
+function stopSpammer(index){
+	clearInterval(GLOBAL.chatSpammer[index].inverval);
+	GLOBAL.chatSpammer.pop(i);
+	GLOBAL.numActivesInterval--;
+}
+
+function stopAllSpammers(){
+	for(var i = 0; i < GLOBAL.numActivesInterval; i++){
+		clearInterval(GLOBAL.chatSpammer[i].inverval);
+	}
+	GLOBAL.chatSpammer = [];
+	GLOBAL.numActivesInterval = 0;
+}
+
+function listSpammers(){
+	console.log('* List of Spammers Actives *');
+	for(var i = 0; i < GLOBAL.numActivesInterval; i++){
+		console.log((i+1)+') '+GLOBAL.chatSpammer[i].intervalTitle);
+		console.log('_____________________________');
+	}
+}
+
+function numberOfSpammers(){
+	console.log('Number of spammers actives: ',GLOBAL.numActivesInterval);
 }
