e87f7260e91298abfc463446cb40fa0b41505615
armanithiago <armani.thiago@gmail.com>
2015-09-27 20:16:25 -0300

    Little changes to a firstTime validation

--- twitch-chat.js
+++ twitch-chat.js
@@ -4,61 +4,31 @@ var GLOBAL = {
 	firstTime : true
 }
 
-/* Spammer */
-
-function startSpammer(text,seconds){
-	var secondsInterval = seconds*1000;
-	var string = text;
-	GLOBAL.chatSpammer.push({inverval:setInterval(function() {
-		$('#chat_text_input').val(string);
-		$('[class="button primary float-right send-chat-button"]').click();
-	}, secondsInterval),intervalTitle:string});
-	GLOBAL.numActivesInterval++;
-}
-
-function stopSpammer(index){
-	clearInterval(GLOBAL.chatSpammer[index].inverval);
-	GLOBAL.chatSpammer.pop(i);
-	GLOBAL.numActivesInterval--;
-}
-
-function stopAllSpammers(){
-	for(var i = 0; i < GLOBAL.numActivesInterval; i++){
-		clearInterval(GLOBAL.chatSpammer[i].inverval);
-	}
-	GLOBAL.chatSpammer = [];
-	GLOBAL.numActivesInterval = 0;
-}
-
-function listSpammers(){
-	console.log('* List of Spammers Actives *');
-	for(var i = 0; i < GLOBAL.numActivesInterval; i++){
-		console.log((i+1)+') '+GLOBAL.chatSpammer[i].intervalTitle);
-		console.log('_____________________________');
-	}
-}
-
-function numberOfSpammers(){
-	console.log('Number of spammers actives: ',GLOBAL.numActivesInterval);
-}
+/* Chat Commands */
 
 $('.chat-lines').on('DOMSubtreeModified', function(){
     if(GLOBAL.firstTime){
-        GLOBAL.firstTime = false;
         var text = $('.chat-lines').find('.message').last().text().trim();
-        verifyCommand(text);
+        var from = $('.chat-lines').find('.from').last().text().trim();
+        verifyCommand(text,from);
         setTimeout(function() { GLOBAL.firstTime = true; }, 100);
     }
 });
 
-/* Chat Commands */
-
-function verifyCommand(text){
+function verifyCommand(text,from){
     var command = text.split(' ');
     if(command[0] == "!isStreaming"){
+    	GLOBAL.firstTime = false;
         getStream(command[1]);
     }else if(command[0] == "!inChat"){
+    	GLOBAL.firstTime = false;
         getChatters(command[1],command[2],command[3]);
+    }else if(command[0] == "!rapebot"){
+    	GLOBAL.firstTime = false;
+        callRapebot();
+    }else if(command[2] == "subscribed!" && from == "twitchnotify"){
+    	GLOBAL.firstTime = false;
+        welcomeSub(command[0]);
     }else{
         return;
     }
@@ -66,30 +36,30 @@ function verifyCommand(text){
 
 function getStream(text){
     var testTrump = text;
-    if(testTrump.toUpperCase() == 'TRUMP'){
+    if(testTrump.toUpperCase() == 'TRUMPSC'){
         returnString = "/me Bot MrDestructoid : Tuck Frump!";
         $('#chat_text_input').val(returnString);
-        setTimeout(function(){ $('.send-chat-button').click(); }, 750);
+        setTimeout(function(){ $('.send-chat-button').click(); }, 500);
     }else{
         $.ajax({
             url:'https://api.twitch.tv/kraken/streams/'+text,
             success:function(data){
                 if(data.stream == null){
                     $('#chat_text_input').val("/me Bot MrDestructoid : "+text+' is not streaming...');
-                    setTimeout(function(){ $('.send-chat-button').click(); }, 750);
+                    setTimeout(function(){ $('.send-chat-button').click(); }, 500);
                 }else{
                     var display_name = data.stream.channel.display_name;
                     var game = data.stream.game;
                     var viewers = data.stream.viewers;
-                    var returnString = "/me Bot MrDestructoid : "+display_name+" is online playing "+game+" with "+viewers+" viewers!";
+                    var returnString = "/me MrDestructoid : "+display_name+" is online playing "+game+" with "+viewers+" viewers!";
                     $('#chat_text_input').val(returnString);
-                    setTimeout(function(){ $('.send-chat-button').click(); }, 750);
+                    setTimeout(function(){ $('.send-chat-button').click(); }, 500);
 
                 }
             },
             error:function(e){
-                $('#chat_text_input').val("/me Bot MrDestructoid : "+text+' don\'t exist...');
-                setTimeout(function(){ $('.send-chat-button').click(); }, 750);
+                $('#chat_text_input').val("/me MrDestructoid : "+text+' don\'t exist...');
+                setTimeout(function(){ $('.send-chat-button').click(); }, 500);
             }
         })  
     }
@@ -107,14 +77,14 @@ function getChatters(streamer,type,user){
             var moderators = json.data.chatters.moderators.length;
             var viewers = json.data.chatters.viewers.length;
             if(type == undefined){
-                var returnString = "/me Bot MrDestructoid : In "+streamer+"'s chat now has "+admins+" admin(s), "+staff+" staff(s), "+global_mods+" global_mod(s), "+moderators+" moderator(s) and "+viewers+" viewers(s).";  
+                var returnString = "/me MrDestructoid : In "+streamer+"'s chat now has "+admins+" admin(s), "+staff+" staff(s), "+global_mods+" global_mod(s), "+moderators+" moderator(s) and "+viewers+" viewers(s).";  
                 $('#chat_text_input').val(returnString);
-                setTimeout(function(){ $('.send-chat-button').click(); }, 750);
+                setTimeout(function(){ $('.send-chat-button').click(); }, 500);
             }else{
                 var typeLower = type.toLowerCase();
                 var exist = false;
                 var valid = false;
-                var returnString = '/me Bot MrDestructoid : ';
+                var returnString = '/me MrDestructoid : ';
                 if(typeLower == 'admin'){
                     valid = true;
                     if(admins != 0){
@@ -174,19 +144,19 @@ function getChatters(streamer,type,user){
                 }
 
                 if(!valid){
-                    $('#chat_text_input').val("/me Bot MrDestructoid : "+streamer+' don\'t exist...');
-                    setTimeout(function(){ $('.send-chat-button').click(); }, 750);
+                    $('#chat_text_input').val("/me MrDestructoid : "+streamer+' don\'t exist...');
+                    setTimeout(function(){ $('.send-chat-button').click(); }, 500);
                 }
                 if(!exist){
                     returnString += 'There are no '+typeLower+' in '+name+'\'s chat.';
                 }
                 $('#chat_text_input').val(returnString);
-                setTimeout(function(){ $('.send-chat-button').click(); }, 750);                
+                setTimeout(function(){ $('.send-chat-button').click(); }, 500);                
             }
         },
         error:function(e){
-            $('#chat_text_input').val("/me Bot MrDestructoid : "+streamer+' don\'t exist...');
-            setTimeout(function(){ $('.send-chat-button').click(); }, 750);
+            $('#chat_text_input').val("/me MrDestructoid : "+streamer+' don\'t exist...');
+            setTimeout(function(){ $('.send-chat-button').click(); }, 500);
         }
     });
 }
@@ -197,8 +167,65 @@ function nameInChatString(data, index, type, name){
         if(i == 0){
             returnString += 'The names of '+type+'(s) in '+name+'\'s chat: '+data[i];
         }else{
-            returnString += ', '+data[i];
+        	if(i < 20){
+            	returnString += ', '+data[i];
+            }else{
+            	returnString += ' and more '+(index-30);
+            	break;
+            }
+
         }
     }
     return returnString;
+}
+
+function callRapebot(){
+	var returnString = "/me MrDestructoid : ヽ༼ຈل͜ຈ༽ﾉ LETS MASTURBATE TOGETHERヽ༼ຈل͜ຈ༽ﾉ";
+	$('#chat_text_input').val(returnString);
+	setTimeout(function(){ $('.send-chat-button').click(); }, 500);
+}
+
+function welcomeSub(subName){
+	var returnString = "/me MrDestructoid : ヽ༼ຈل͜ຈ༽ﾉ WHALE CUM "+subName+"!! ヽ༼ຈل͜ຈ༽ﾉ";
+	$('#chat_text_input').val(returnString);
+	setTimeout(function(){ $('.send-chat-button').click(); }, 500);
+}
+
+
+/* Spammer */
+
+function startSpammer(text,seconds){
+	var secondsInterval = seconds*1000;
+	var string = text;
+	GLOBAL.chatSpammer.push({inverval:setInterval(function() {
+		$('#chat_text_input').val(string);
+		$('[class="button primary float-right send-chat-button"]').click();
+	}, secondsInterval),intervalTitle:string});
+	GLOBAL.numActivesInterval++;
+}
+
+function stopSpammer(index){
+	clearInterval(GLOBAL.chatSpammer[index].inverval);
+	GLOBAL.chatSpammer.pop(i);
+	GLOBAL.numActivesInterval--;
+}
+
+function stopAllSpammers(){
+	for(var i = 0; i < GLOBAL.numActivesInterval; i++){
+		clearInterval(GLOBAL.chatSpammer[i].inverval);
+	}
+	GLOBAL.chatSpammer = [];
+	GLOBAL.numActivesInterval = 0;
+}
+
+function listSpammers(){
+	console.log('* List of Spammers Actives *');
+	for(var i = 0; i < GLOBAL.numActivesInterval; i++){
+		console.log((i+1)+') '+GLOBAL.chatSpammer[i].intervalTitle);
+		console.log('_____________________________');
+	}
+}
+
+function numberOfSpammers(){
+	console.log('Number of spammers actives: ',GLOBAL.numActivesInterval);
 }
